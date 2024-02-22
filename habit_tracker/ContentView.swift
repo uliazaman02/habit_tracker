643429146ff6c4707699cf4102da19694152a040
uliazaman02 <ulia.zaman02@gmail.com>
2024-02-21 23:51:17 -0800

    more buttons and setup

--- habit_tracker/ContentView.swift
+++ habit_tracker/ContentView.swift
@@ -9,24 +9,66 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var newHabit: String = ""
+    @State private var newHabitInput: String = ""
+    @State private var allHabits: [String] = []
+    @State private var mainButton: Bool = false
+    @State private var childButton: Bool = false
     
     var body: some View {
         VStack {
             Text("Habit Tracker")
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(.title)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 .padding()
             
             Spacer()
             
-            TextField("New Habit", text: $newHabit)
+            if mainButton && !allHabits.isEmpty {
+                VStack {
+                    HStack {
+                        Spacer()
+                    }
+                    
+                    ForEach(allHabits, id: \.self) {
+                        text in HStack {
+                            Text(text)
+                                .padding()
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5.0)
+                                        .stroke(Color.pink, lineWidth: 2)
+                                )
+                            
+                            Button(action: {
+                                childButton.toggle()
+                            }) {
+                                if childButton {
+                                    Image(systemName: "circle.fill")
+                                        .foregroundColor(.pink)
+                                }
+                                else {
+                                    Image(systemName: "circle.fill")
+                                        .foregroundColor(.white)
+                                        .background(.pink)
+                                        .clipShape(Circle())
+                                }
+                            }
+                        }
+                    }
+                }
+                    .padding()
+                    .background(Color.pink.opacity(0.3))
+            }
+            
+            TextField("New Habit", text: $newHabitInput)
                 .padding()
-                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .background(Color.pink.opacity(0.3))
             
             Button("Add New Habit") {
-                if !newHabit.isEmpty {
-                    Text("\(newHabit)")
-                }
+                mainButton = true
+                allHabits.append(newHabitInput)
+                newHabitInput = ""
             }
                 .buttonStyle(.borderedProminent)
             
