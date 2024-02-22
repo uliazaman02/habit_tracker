c4ffe96d7091c452ba15aedd7905cd57ab3d107d
uliazaman02 <ulia.zaman02@gmail.com>
2024-02-21 20:59:50 -0800

    added title, text field, button

--- habit_tracker/ContentView.swift
+++ habit_tracker/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var newHabit: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Habit Tracker")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .padding()
+            
+            Spacer()
+            
+            TextField("New Habit", text: $newHabit)
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+            
+            Button("Add New Habit") {
+                if !newHabit.isEmpty {
+                    Text("\(newHabit)")
+                }
+            }
+                .buttonStyle(.borderedProminent)
+            
+            Spacer()
         }
-        .padding()
+        
+//        Image(systemName: "globe")
+//            .imageScale(.large)
+//            .foregroundStyle(.tint)
     }
 }
 
