0952102b5556fed311829a87937c928bb08d4d6f
uliazaman02 <ulia.zaman02@gmail.com>
2024-02-21 20:12:47 -0800

    Initial Commit

---
+++ habit_tracker/habit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackerApp.swift
+//  habit_tracker
+//
+//  Created by Ulia on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
